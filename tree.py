2a8e9932f7c6062b3d9f905b2e3ff85d9156c55e
Kristian Koehntopp <kris-git@koehntopp.de>
2024-02-27 12:48:11 +0100

    Wir wollen die auch drucken können.

--- tree.py
+++ tree.py
@@ -10,3 +10,20 @@ class Node:
         self.left = left
         self.right = right
 
+    def __repr__(self):
+        """ Return a string representation of the node. """
+        left = "None"
+        right = "None"
+        if self.left is not None:
+            left = self.left.value
+        if self.right is not None:
+            right = self.right.value
+
+        # Was passiert, wenn wir stattdessen {self.left} und {self.right} einsetzen würden? -> Rekursion (aber versteckt)
+        return f"Node(value={self.value}, left={left}, right={right})"
+
+
+if __name__ == "__main__":
+    # Wir machen uns eine einsame Node n
+    n = Node(1)
+    print(f"{n=}")
