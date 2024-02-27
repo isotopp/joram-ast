0b84d64111516e188fcbe485e610753af29c40ee
Kristian Koehntopp <kris-git@koehntopp.de>
2024-02-27 12:22:57 +0100

    Jetzt mit Tiefenzähler

--- fac.py
+++ fac.py
@@ -21,5 +21,21 @@ def fac(n: int) -> int:
     return n * fac(n - 1)
 
 
+def fac_depth(n: int, depth: int = 0) -> int:
+    sp = '  ' * depth
+    print(f"{sp}Call to fac_depth({n=}, {depth=})")
+    if n < 1:
+        raise ValueError(f"n={n} must be greater than or equal to 1")
+
+    if n == 1:
+        print(f"{sp}return 1")
+        return 1
+
+    r = n * fac_depth(n - 1, depth + 1)
+    print(f"{sp}return {r}")
+    return r
+
+
 if __name__ == "__main__":
     print(fac(5))
+    print(fac_depth(5))
