a94f5384c3624227b5dcdaa9c7fb4debd47c3d5c
Kristian Koehntopp <kris-git@koehntopp.de>
2024-02-27 12:24:59 +0100

    Alles ohne Debug

--- fac.py
+++ fac.py
@@ -11,7 +11,7 @@
 #   1 * (120) = 120
 
 def fac(n: int) -> int:
-    print(f"Call to fac({n})")
+    # print(f"Call to fac({n})")
     if n < 1:
         raise ValueError(f"n={n} must be greater than or equal to 1")
 
@@ -22,33 +22,33 @@ def fac(n: int) -> int:
 
 
 def fac_depth(n: int, depth: int = 0) -> int:
-    sp = '  ' * depth
-    print(f"{sp}Call to fac_depth({n=}, {depth=})")
+    # sp = '  ' * depth
+    # print(f"{sp}Call to fac_depth({n=}, {depth=})")
     if n < 1:
         raise ValueError(f"n={n} must be greater than or equal to 1")
 
     if n == 1:
-        print(f"{sp}return 1")
+        # print(f"{sp}return 1")
         return 1
 
-    r = n * fac_depth(n - 1, depth + 1)
-    print(f"{sp}return {r}")
-    return r
+    # r = n * fac_depth(n - 1, depth + 1)
+    # print(f"{sp}return {r}")
+    return n * fac_depth(n - 1, depth + 1)
 
 
 def fac_tr(n, akku=1, depth=0):
-    sp = '  ' * depth
-    print(f"{sp}Call to fac_tr({n=}, {akku=}, {depth=})")
+    # sp = '  ' * depth
+    # print(f"{sp}Call to fac_tr({n=}, {akku=}, {depth=})")
     if n < 1:
         raise ValueError(f"n={n} must be greater than or equal")
 
     if n == 1:
-        print(f"{sp}return 1")
+        # print(f"{sp}return 1")
         return akku
     else:
-        r = fac_tr(n - 1, n * akku, depth + 1)
-        print(f"{sp}return {r} = fac_tr({n=}, {akku=}, {depth=}")
-        return r
+        # r = fac_tr(n - 1, n * akku, depth + 1)
+        # print(f"{sp}return {r} = fac_tr({n=}, {akku=}, {depth=}")
+        return fac_tr(n - 1, n * akku, depth + 1)
 
 
 if __name__ == "__main__":
